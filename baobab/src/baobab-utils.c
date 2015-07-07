fb7a22b4c64322f57ce6c91855032730151e688f
Monsta <monsta@inbox.ru>
2015-07-07 11:55:25 +0300

    baobab: show correct help page in prefs dialog

--- baobab/src/baobab-utils.c
+++ baobab/src/baobab-utils.c
@@ -507,7 +507,7 @@ baobab_help_display (GtkWindow   *parent,
 	gboolean ret;
 
 	uri = (link_id) ? 
-		g_strdup_printf ("help:%s#%s", file_name, link_id) :
+		g_strdup_printf ("help:%s/%s", file_name, link_id) :
 		g_strdup_printf ("help:%s", file_name);
 
 	ret = gtk_show_uri (gtk_window_get_screen (parent),
