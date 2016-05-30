3f63b98946169bf9f7b4013376113af5bc107605
raveit65 <chat-to-me@raveit.de>
2016-05-30 17:21:26 +0200

    Gtk+3 mdict sidebar: do not use derecated GtkArrow
    taken from:
    https://git.gnome.org/browse/gnome-dictionary/commit/?id=c60d8c8

--- mate-dictionary/src/gdict-sidebar.c
+++ mate-dictionary/src/gdict-sidebar.c
@@ -367,7 +367,11 @@ gdict_sidebar_init (GdictSidebar *sidebar)
   gtk_box_pack_start (GTK_BOX (select_hbox), priv->label, FALSE, FALSE, 0);
   gtk_widget_show (priv->label);
 
+#if GTK_CHECK_VERSION (3, 0, 0)
+  arrow = gtk_image_new_from_icon_name ("go-down-symbolic", GTK_ICON_SIZE_BUTTON);
+#else
   arrow = gtk_arrow_new (GTK_ARROW_DOWN, GTK_SHADOW_NONE);
+#endif
   gtk_box_pack_end (GTK_BOX (select_hbox), arrow, FALSE, FALSE, 0);
   gtk_widget_show (arrow);
 
