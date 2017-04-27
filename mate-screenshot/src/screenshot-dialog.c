mate-desktop/mate-utils

commit 7d859a9e17f0c4f0b790e8b2a3d8ac25481d7fe6

avoid deprecated gdk_cursor_new ()

mate-screenshot/src/screenshot-dialog.c
@@ -408,14 +408,16 @@ screenshot_dialog_set_busy (ScreenshotDialog *dialog,
 			    gboolean          busy)
 {
   GtkWidget *toplevel;
+  GdkDisplay *display;
 
   toplevel = screenshot_dialog_get_toplevel (dialog);
+  display = gtk_widget_get_display (GTK_WIDGET (dialog));
 
   if (busy)
     {
       GdkCursor *cursor;
       /* Change cursor to busy */
-      cursor = gdk_cursor_new (GDK_WATCH);
+      cursor = gdk_cursor_new_for_display (display, GDK_WATCH);
       gdk_window_set_cursor (gtk_widget_get_window (toplevel), cursor);
 #if GTK_CHECK_VERSION (3, 0, 0)
       g_object_unref (cursor);

gsearchtool/src/gsearchtool-callbacks.c
@@ -1421,10 +1421,13 @@ file_motion_notify_cb (GtkWidget *widget,
                        GdkEventMotion *event,
                        gpointer user_data)
 {
-        GSearchWindow * gsearch = user_data;
+	GSearchWindow * gsearch = user_data;
+	GdkDisplay *display;
 	GdkCursor * cursor;
 	GtkTreePath * last_hover_path;
 	GtkTreeIter iter;
+
+	display = gtk_widget_get_display (GTK_WIDGET (user_data));
 
 	if (gsearch->is_search_results_single_click_to_activate == FALSE) {
 		return FALSE;
@@ -1442,7 +1445,7 @@ file_motion_notify_cb (GtkWidget *widget,
 				       NULL, NULL, NULL);
 
 	if (gsearch->search_results_hover_path != NULL) {
-		cursor = gdk_cursor_new (GDK_HAND2);
+		cursor = gdk_cursor_new_for_display (display, GDK_HAND2);
 	}
 	else {
 		cursor = NULL;

mate-dictionary/src/gdict-window.c
@@ -316,13 +316,16 @@ static void
 gdict_window_lookup_start_cb (GdictContext *context,
 			      GdictWindow  *window)
 {
+  GdkDisplay *display;
   gchar *message;
+
+  display = gtk_widget_get_display (GTK_WIDGET (window));
 
   if (!window->word)
     return;
 
   if (!window->busy_cursor)
-    window->busy_cursor = gdk_cursor_new (GDK_WATCH);
+    window->busy_cursor = gdk_cursor_new_for_display (display, GDK_WATCH);
 
   message = g_strdup_printf (_("Searching for '%s'..."), window->word);
   

mate-screenshot/src/screenshot-utils.c
@@ -499,6 +499,7 @@ emit_select_callback_in_idle (gpointer user_data)
 void
 screenshot_select_area_async (SelectAreaCallback callback)
 {
+  GdkDisplay *display;
   GdkCursor               *cursor;
 #if GTK_CHECK_VERSION (3, 0, 0)
   GdkDeviceManager *manager;
@@ -524,10 +525,11 @@ screenshot_select_area_async (SelectAreaCallback callback)
   g_signal_connect (data.window, "button-release-event", G_CALLBACK (select_area_button_release), &data);
   g_signal_connect (data.window, "motion-notify-event", G_CALLBACK (select_area_motion_notify), &data);
 
-  cursor = gdk_cursor_new (GDK_CROSSHAIR);
+  display = gdk_display_get_default ();
+  cursor = gdk_cursor_new_for_display (display, GDK_CROSSHAIR);
 
 #if GTK_CHECK_VERSION (3, 0, 0)
-  manager = gdk_display_get_device_manager (gdk_display_get_default ());
+  manager = gdk_display_get_device_manager (display);
   pointer = gdk_device_manager_get_client_pointer (manager);
   keyboard = gdk_device_get_associated_device (pointer);
 

commit 5fa0c1c4ad56a0d28b99b3a8dcebdb6cb0ad2ce0

mate-screenshot: fix showing busy cursor when saving area screenshot
invalid cast from plain struct to GtkWidget was causing a heap
of runtime warnings when saving area screenshot, and no busy cursor
was shown. fallout from 7d859a9e17f0c4f0b790e8b2a3d8ac25481d7fe6...

mate-screenshot/src/screenshot-dialog.c
@@ -355,7 +355,7 @@ screenshot_dialog_set_busy (ScreenshotDialog *dialog,
   GdkDisplay *display;
 
   toplevel = screenshot_dialog_get_toplevel (dialog);
-  display = gtk_widget_get_display (GTK_WIDGET (dialog));
+  display = gtk_widget_get_display (toplevel);
 
   if (busy)
     {