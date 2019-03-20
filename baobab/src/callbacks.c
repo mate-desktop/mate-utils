637f9b766760d959dabb75a9b677364a83a61cd0
rbuj <robert.buj@gmail.com>
2019-03-20 18:13:45 +0100

    mate-dictionary: Use g_strjoin to build license text
    full stop at end of sentence in license paragraphs

--- baobab/src/callbacks.c
+++ baobab/src/callbacks.c
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor,
- * Boston, MA  02110-1301  USA
+ * Boston, MA  02110-1301  USA.
  */
 
 #ifdef HAVE_CONFIG_H
@@ -85,7 +85,7 @@ on_about_activate (GtkMenuItem *menuitem, gpointer user_data)
 
 		N_("You should have received a copy of the GNU General Public License "
 		"along with this program; if not, write to the Free Software Foundation, Inc., 51 "
-		"Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA")
+		"Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.")
 	};
 
 
