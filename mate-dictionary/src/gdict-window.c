f93cd6caf148bc1566d09b511522cd61950947f7
rbuj <robert.buj@gmail.com>
2022-01-01 11:04:26 +0100

    mate-dictionary: fix memory leak

--- mate-dictionary/src/gdict-window.c
+++ mate-dictionary/src/gdict-window.c
@@ -127,7 +127,6 @@ gdict_window_dispose (GObject *gobject)
   if (window->settings != NULL)
     {
       g_object_unref (window->settings);
-
       window->settings = NULL;
     }
 
@@ -185,6 +184,8 @@ gdict_window_dispose (GObject *gobject)
       g_object_unref (window->busy_cursor);
       window->busy_cursor = NULL;
     }
+
+  g_clear_pointer (&window->sidebar_page, g_free);
 
   G_OBJECT_CLASS (gdict_window_parent_class)->dispose (gobject);
 }
