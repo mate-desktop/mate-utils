22eae24a9ee6df7b001c8ad0b0fc8d8644f7bb12
mbkma <johannes.unruh@fau.de>
2026-01-28 09:00:33 +0100

    ci: add missing PKGBUILD depends to archlinux CI script

--- .github/workflows/archlinux.sh
+++ .github/workflows/archlinux.sh
@@ -11,25 +11,31 @@ infoend() {
 # Required packages on Archlinux
 requires=(
 	ccache # Use ccache to speed up build
+	clang  # Build with clang on Archlinux
 )
 
+# https://gitlab.archlinux.org/archlinux/packaging/packages/mate-utils
 requires+=(
 	autoconf-archive
 	gcc
+	gettext
 	git
+	glib2-devel
+	inkscape
+	intltool
 	itstool
 	libcanberra
 	libgtop
+	libsm
+	libxml2
 	make
 	mate-common
+	mate-desktop
 	mate-panel
 	python
 	udisks2
 	which
 	yelp-tools
-	dconf
-	gobject-introspection
-	iso-codes
 )
 
 infobegin "Update system"
